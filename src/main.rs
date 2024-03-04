a092fbd0b59c0b6311c5a853125052a47b9b5df8
K3cs1 <kecskemetip@gmail.com>
2024-03-04 21:09:38 +0100

    Basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::rc::Rc;
 
 use log::info;
-use slint::{Model, VecModel, Weak};
+use slint::{Brush, Color, Model, VecModel, Weak};
+
+const PLAYER_WIN_COLOR: Brush = Brush::SolidColor(Color::from_rgb_u8(0, 140, 0));
+const COMPUTER_WIN_COLOR: Brush = Brush::SolidColor(Color::from_rgb_u8(140, 0, 0));
 
 //slint::include_modules!();
 
@@ -27,31 +30,45 @@ fn main() {
     ui.on_computer_turn(move |id: i32| {
         info!("{}", id);
 
-        let ui: AppWindow = ui_weak.unwrap();
+        //let ui: AppWindow = ui_weak.unwrap();
 
         let mut empty_tiles = tiles_model
             .iter()
             .enumerate()
             .filter(|(_, tile)| tile.empty);
 
+        //Player turn    
         empty_tiles.for_each(|(_i, mut tile_data)| {
             if id == tile_data.id {
                 tile_data.player_clicked = true;
-                //tile_data.computer_clicked = true;
                 tile_data.empty = false;
+                //tile_data.win_color = COMPUTER_WIN_COLOR;
                 tiles_model.set_row_data(_i, tile_data);
             }
         });
 
-        // if let Some((tile_idx, mut tile_data)) =
-        //     empty_tiles.next()
-        // {
-        //     if tile_data.id == id {
-        //         tile_data.player_clicked = true;
-        //         tile_data.empty = false;
-        //         tiles_model.set_row_data(tile_idx, tile_data);
-        //     }
-        // }
+        //Computer turn
+        let mut new_empty_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.empty);
+
+        let mut free_ids = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.empty)
+            .map(|(_, tile_data)| tile_data.id);
+
+        let computer_id = free_ids.next().unwrap();
+        new_empty_tiles.for_each(|(_i, mut tile_data)| {
+            if computer_id == tile_data.id {
+                tile_data.computer_clicked = true;
+                tile_data.empty = false;
+                //tile_data.win_color = COMPUTER_WIN_COLOR;
+                tiles_model.set_row_data(_i, tile_data);            
+            }
+        });
+
     });
 
     ui.run().unwrap();
