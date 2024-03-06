bdd1e1cb540669c859d164a38263484e67dead83
K3cs1 <kecskemetip@gmail.com>
2024-03-06 08:13:39 +0100

    WinTree impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 use log::info;
 use slint::{Brush, Color, Model, VecModel, Weak};
@@ -12,6 +12,123 @@ slint::slint! {
     import { AppWindow } from "ui/appwindow.slint";
 }
 
+enum Player {
+    Machine,
+    Human,
+    Nobody,
+}
+
+struct Tile {
+    field_id: i32,
+    player: Player,
+}
+
+impl Tile {
+    fn init() -> Self {
+        Self {
+            field_id: -1,
+            player: Player::Nobody,
+        }
+    }
+
+    fn new(field_id: i32, player: Player) -> Self {
+        Self { field_id, player }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct WinTree<T> {
+    value: T,
+    left: Option<Box<WinTree<T>>>,
+    right: Option<Box<WinTree<T>>>,
+}
+
+impl<T> WinTree<T> {
+    pub fn new(value: T) -> Self {
+        WinTree {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+    pub fn left(mut self, node: WinTree<T>) -> Self {
+        self.left = Some(Box::new(node));
+        self
+    }
+
+    pub fn right(mut self, node: WinTree<T>) -> Self {
+        self.right = Some(Box::new(node));
+        self
+    }
+}
+
+fn init_win_tree() -> WinTree<Vec<Tile>> {
+    let mut win_tree = WinTree::new(vec![Tile::init()])
+        .left(WinTree::new(vec![Tile::new(4, Player::Machine)]))
+        .left(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+        ]))
+        .left(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+        ]))
+        .right(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(2, Player::Human),
+        ]))
+        .right(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(2, Player::Human),
+            Tile::new(1, Player::Machine),
+        ]))
+        .right(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(2, Player::Human),
+            Tile::new(1, Player::Machine),
+            Tile::new(8, Player::Human),
+        ]))
+        .right(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(2, Player::Human),
+            Tile::new(1, Player::Machine),
+            Tile::new(8, Player::Human),
+            Tile::new(3, Player::Machine),
+        ]))
+        .left(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(0, Player::Human),
+        ]))
+        .left(WinTree::new(vec![
+            Tile::new(4, Player::Machine),
+            Tile::new(1, Player::Human),
+            Tile::new(6, Player::Machine),
+            Tile::new(0, Player::Human),
+            Tile::new(2, Player::Machine),
+        ]));
+
+    win_tree
+}
+
+fn has_machine_win(tiles_model: Rc<VecModel<TileData>>) -> bool {
+    todo!()
+}
+
+fn has_human_win(tiles_model: Rc<VecModel<TileData>>) -> bool {
+    todo!()
+}
+
 fn main() {
     //-> Result<(), slint::PlatformError>
     std::env::set_var("RUST_LOG", "debug");
@@ -23,26 +140,31 @@ fn main() {
     let mut ttt_tiles: Vec<TileData> = ui.get_ttt_tiles().iter().collect();
     ttt_tiles.extend(ttt_tiles.clone());
 
-    let tiles_model = Rc::new(VecModel::from(ttt_tiles));
+    let tiles_model: Rc<VecModel<TileData>> = Rc::new(VecModel::from(ttt_tiles));
 
     ui.set_ttt_tiles(tiles_model.clone().into());
 
-    ui.on_computer_turn(move |id: i32| {
+    let win_tree: WinTree<Vec<Tile>> = init_win_tree();
+
+    ui.on_process(move |id: i32| {
         info!("{}", id);
 
         //let ui: AppWindow = ui_weak.unwrap();
+
+        has_human_win(tiles_model.clone());
+        
+        has_machine_win(tiles_model.clone());
 
         let mut empty_tiles = tiles_model
             .iter()
             .enumerate()
             .filter(|(_, tile)| tile.empty);
 
-        //Player turn    
+        //Human turn
         empty_tiles.for_each(|(_i, mut tile_data)| {
             if id == tile_data.id {
                 tile_data.player_clicked = true;
                 tile_data.empty = false;
-                //tile_data.win_color = COMPUTER_WIN_COLOR;
                 tiles_model.set_row_data(_i, tile_data);
             }
         });
@@ -65,10 +187,9 @@ fn main() {
                 tile_data.computer_clicked = true;
                 tile_data.empty = false;
                 //tile_data.win_color = COMPUTER_WIN_COLOR;
-                tiles_model.set_row_data(_i, tile_data);            
+                tiles_model.set_row_data(_i, tile_data);
             }
-        });
-
+        });        
     });
 
     ui.run().unwrap();
